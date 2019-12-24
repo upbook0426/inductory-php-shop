eea60b3327d284d5201f72e9ebcb2958aa0e3b73
upbook0426 <58377749+upbook0426@users.noreply.github.com>
2019-12-24 22:08:18 +0900

    Add files via upload

---
+++ staff/staff_edit_done.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['login'])==false)
+{
+	print'ログインされていません。<br/>';
+	print'<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+	exit();
+}
+else
+{
+	print $_SESSION['staff_name'];
+	print'さんログイン中<br/>';
+	print'<br/>';
+}
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>ろくまる農園</title>
+	</head>
+	<body>
+	<?php
+	
+		try
+		{
+	$staff_name = $_POST['name'];
+	$staff_code = $_POST['code'];
+	$staff_pass = $_POST['pass'];
+	
+	$staff_name =htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+	$staff_pass =htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+	
+	$dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
+	$user ='root';
+	$password='';
+	$dbh=new PDO($dsn,$user,$password);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+	$sql='UPDATE mst_staff SET name=?,password=? WHERE code=?'; 
+	$stmt=$dbh->prepare($sql);
+	$data[] = $staff_name;
+	$data[] = $staff_pass;
+	$data[] = $staff_code;
+	$stmt->execute($data);
+
+	$dbh = null;
+
+	
+			}
+	
+	catch (Exception $e)
+
+{
+	print'ただいま障害により大変ご迷惑をお掛けしております。';
+	exit();
+			
+}
+	
+		?>
+		修正しました。<br/>
+		<br/>
+		<a href="staff_list.php">戻る</a>
+	
+			</body>
+</html>
